2b79a9bb7c0f7a813b7d0732db238e23853255ec
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2024-07-14 02:25:42 +0200

    projet WFES wireframe on knx online for demo and feedback

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -52,7 +52,7 @@ function Nav() {
           <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
             <li>
               <NavLink
-                to="/home"
+                to="/WFES-V2"
                 className={({ isActive }) => (isActive ? "active" : "")}
                 onClick={closeMenu}
               >
